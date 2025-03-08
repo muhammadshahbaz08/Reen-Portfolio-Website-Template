08c4a2008bbba3ed45a0d3cacb58023e41d10bc3
Muhammad Shahbaz <muhamadshahbaz17@gmail.com>
2025-03-08 15:56:54 +0500

    📦 NEW: Services Section Added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,8 +4,8 @@ export default function Home() {
   return (
     <main >
       <Hero />
-      {/* <Services />
-      <About /> */}
+       <Services />
+      {/* <About />  */}
     </main>
   );
 }
