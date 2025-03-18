ec7b0634fa32d87dc58506927c07608f747213af
Muhammad Shahbaz <muhamadshahbaz17@gmail.com>
2025-03-18 22:59:16 +0500

    📦 NEW: CallToAction Section Added

---
+++ src/app/components/CallToAction.tsx
@@ -0,0 +1,28 @@
+const CallToAction = () => {
+  return (
+    <section className="container max-w-[1160px] mx-auto  py-32">
+      {/*section-content */}
+      <div className="w-[745px] mx-auto text-center ">
+        <h1 className="text-[#506a85] text-[44px] mb-10 font-semibold">
+          Want to work with us?
+        </h1>
+        <p className="text-[#73879c] text-[22px] ">
+          Magnis modipsae que voloratati andigen daepeditem quiate re porem aut
+          labor. Laceaque quiae sitiorem rest non restibusaes maio es dem
+          tumquam.
+        </p>
+      </div>
+      {/* CTA button */}
+      <div className="mt-12 flex justify-center">
+        <a
+          href=""
+          className="text-white bg-[#00bc9c] hover:bg-[#05836e] font-bold text-[18px] border-b-2 border-[#00a085] rounded-md py-4 px-9"
+        >
+          GET IN TOUCH
+        </a>
+      </div>
+    </section>
+  );
+};
+
+export default CallToAction;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-import { Hero, Services, Portfolio } from "./components";
+import { Hero, Services, Portfolio, CallToAction } from "./components";
 
 export default function Home() {
   return (
@@ -6,6 +6,7 @@ export default function Home() {
       <Hero />
       <Services />
       <Portfolio />
+      <CallToAction />
     </main>
   );
 }
